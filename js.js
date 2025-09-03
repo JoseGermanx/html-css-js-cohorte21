6f52e7daca328c39e0ef7844924960b3a1d0a3c9
José G. Martínez <jgermanmm@gmail.com>
2025-09-03 13:04:49 -0400

    Añadir estructura HTML y estilos básicos, implementar funciones de JavaScript para interactividad

---
+++ js.js
@@ -0,0 +1,10 @@
+
+console.log("Hola estudiantes, desde un archivo JS externo")
+
+function aumentarTamano() {
+    document.getElementById("principal").style.fontSize = "xx-large"
+}
+
+function cambiarTextoTitulo() {
+    document.getElementById("x-change").innerHTML =" Texto de Título Cambiado"
+}
